090ffd1baa66702e23abc1133dbab61e4876c5df
Ramneet04 <ramneetsinghrtd@gmail.com>
2024-11-15 01:06:38 +0530

    db initialize

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
 
 
 export const router = Router();
@@ -16,6 +19,15 @@ router.post("/signin", (req,res)=>{
     })
 })
 
+router.get("/element", (req,res)=>{
+
+})
+
+router.get("/avatars", (req,res)=>{
+
+})
 
 
-// router.use("/user", userRouter);
+router.use("/user", userRouter);
+router.use("/space", spaceRouter);
+router.use("/admin", adminRouter);
