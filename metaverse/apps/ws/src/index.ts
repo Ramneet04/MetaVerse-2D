30372d27d247d569b45055eb96e6e45b2e8e2c66
Ramneet04 <ramneetsinghrtd@gmail.com>
2024-11-24 00:24:03 +0530

    setting ws backend

--- metaverse/apps/ws/src/index.ts
+++ metaverse/apps/ws/src/index.ts
@@ -3,9 +3,7 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 3001});
 
 wss.on("connection", function connection(ws){
+    console.log("Client connected");
     ws.on("error", console.error);
-    ws.on("message", function message(data){
-        console.log(`Received message => ${data}`)
-    })
-    ws.send("something");
+    
 })
